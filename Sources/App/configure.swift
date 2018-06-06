2108a429eeef07127f69b40bd91974b796fe6e59
Rocky <542622608@qq.com>
2018-06-06 11:06:19 +0800

    leaf 简单使用

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,5 +1,6 @@
 import FluentPostgreSQL
 import Vapor
+import Leaf
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
@@ -58,19 +59,8 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     var commandConfig = CommandConfig.default()
     commandConfig.use(RevertCommand.self, as: "revert")
     services.register(commandConfig)
-    
-//    // Configure a SQLite database
-//    let sqlite = try SQLiteDatabase(storage: .memory)
-//
-//    /// Register the configured SQLite database to the database config.
-//    var databases = DatabasesConfig()
-//    databases.add(database: sqlite, as: .sqlite)
-//    services.register(databases)
-//
-//    /// Configure migrations
-//    var migrations = MigrationConfig()
-////    migrations.add(model: Todo.self, database: .sqlite)
-//    migrations.add(model: Acronym.self, database: .sqlite)
-//    services.register(migrations)
+
+    try services.register(LeafProvider())
+    config.prefer(LeafRenderer.self, for: ViewRenderer.self)
 
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,6 +8,7 @@ public func routes(_ router: Router) throws {
     try router.register(collection: AcronymsController())
     try router.register(collection: UsersController())
     try router.register(collection: CategoriesControlelr())
+    try router.register(collection: WebsiteController())
 }
 
 
