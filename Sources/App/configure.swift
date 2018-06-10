96d7583812c41e93f80b96eaf31fd54b97adb13b
Rocky <542622608@qq.com>
2018-06-11 00:18:16 +0800

    抽取了configure方法

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -6,23 +6,31 @@ import Authentication
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
     /// Register providers first
+    try registerProvider(&services, &config)
+    try registerRouter(&services)
+    configureMiddlewares(&services)
+    configureDatabase(&services, &env)
+    configureMigrations(&services)
+    registerCommand(&services)
+}
+
+fileprivate func registerProvider(_ services: inout Services, _ config: inout Config) throws {
+    
     try services.register(FluentPostgreSQLProvider())
+    try services.register(LeafProvider())
+    try services.register(AuthenticationProvider())
     
-    
-    /// Register routes to the router
+    config.prefer(LeafRenderer.self, for: ViewRenderer.self)
+    config.prefer(MemoryKeyedCache.self, for: KeyedCache.self)
+}
+
+fileprivate func registerRouter(_ services: inout Services) throws {
     let router = EngineRouter.default()
     try routes(router)
     services.register(router, as: Router.self)
+}
 
-    /// Register middleware
-    var middlewares = MiddlewareConfig() // Create _empty_ middleware config
-    /// middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
-    middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
-    middlewares.use(FileMiddleware.self)
-    services.register(middlewares)
-    
-    
-    
+fileprivate func configureDatabase(_ services: inout Services, _ env: inout Environment) {
     var databases = DatabasesConfig()
     
     let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
@@ -51,6 +59,10 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     databases.add(database: database, as: .psql)
     services.register(databases)
 
+}
+
+fileprivate func configureMigrations(_ services: inout Services) {
+    
     var migrations = MigrationConfig()
     migrations.add(model: User.self, database: .psql)
     migrations.add(model: Acronym.self, database: .psql)
@@ -59,14 +71,20 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     migrations.add(model: Token.self, database: .psql)
     migrations.add(migration: AdminUser.self, database: .psql)
     services.register(migrations)
+}
+
+fileprivate func configureMiddlewares(_ services: inout Services) {
     
+    var middlewares = MiddlewareConfig() // Create _empty_ middleware config
+    /// middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
+    middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
+    middlewares.use(FileMiddleware.self)
+    middlewares.use(SessionsMiddleware.self)
+    services.register(middlewares)
+}
+
+fileprivate func registerCommand(_ services: inout Services) {
     var commandConfig = CommandConfig.default()
     commandConfig.use(RevertCommand.self, as: "revert")
     services.register(commandConfig)
-
-    ///leaf
-    try services.register(LeafProvider())
-    config.prefer(LeafRenderer.self, for: ViewRenderer.self)
-
-    try services.register(AuthenticationProvider())
 }
