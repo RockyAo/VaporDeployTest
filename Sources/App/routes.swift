36a44a2b891f3c4f6427a0aa67e338d6b802842b
Rocky <542622608@qq.com>
2018-06-13 11:04:20 +0800

    no message

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -9,6 +9,10 @@ public func routes(_ router: Router) throws {
     try router.register(collection: UsersController())
     try router.register(collection: CategoriesControlelr())
     try router.register(collection: WebsiteController())
+    
+    router.get("hello") { (req)  in
+        return "hello world"
+    }
 }
 
 
